7838231be201a285b08bc1b5578e532bc45066b7
Administrator <Administrator@WIN-7R6E5BBL67U>
2019-11-14 18:10:44 +0530

    NewApp

---
+++ TestApp/src/com/jarmous/MasterServlet.java
@@ -0,0 +1,25 @@
+package com.jarmous;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class MasterServlet extends HttpServlet
+{
+	public void service(HttpServletRequest req, HttpServletResponse res ) throws IOException
+	{
+	String p = req.getParameter("email");
+	String q = req.getParameter("pass");
+	
+	if(p == "admin"&& q == "12345678")
+	{
+		
+		res.getWriter().println(" User Identified:" + p);
+	}
+	res.getWriter().println(" Unauthorized user! ");
+	}
+
+}
